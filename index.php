eb3887d2a7ce269c01ac16a46ba0b01e01676842
Knuk <samuellapointe@gmail.com>
2016-11-05 17:10:12 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Bootstrap et Sprites CSS</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    
+    <body>
+        allo
+        <script type="text/javascript" src="js/script.js"></script>
+		<?php include 'test.php';?>
+    </body>
+</html>
