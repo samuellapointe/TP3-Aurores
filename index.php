c0832aa04ee354ef88114d8e174f89f03474d2f7
Samuel Lapointe <samuellapointe@gmail.com>
2016-11-24 10:09:17 -0500

    Add boostrap

--- index.php
+++ index.php
@@ -2,29 +2,48 @@
 <html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>TODO</title>
+        
         <link rel="stylesheet" type="text/css" href="css/style.css" />
 		<link rel="stylesheet" type="text/css" href="css/ol.css" />
 		<script type="text/javascript" src="js/ol.js"></script>
 		<script type="text/javascript" src="js/script.js"></script>
+		
+		<title>TODO</title>
+		
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
     </head>
     
     <body>
-		<!-- This div contains the map, empty at this point -->
-		<div id="map" class="map"></div>
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Aurora</a>
+				</div>
+			</div>
+		</nav>
 		
-		<br />
-		<canvas style="display: none" id="myCanvas" width="1024" height="512"></canvas>
-		<div id="mouse-position"></div>
+		<div id="content">
+			<!-- This div contains the map, empty at this point -->
+			<div id="map" class="map"></div>
+			
+			<br />
+			<canvas style="display: none" id="myCanvas" width="1024" height="512"></canvas>
+			<div id="mouse-position"></div>
+			
+			<!-- Initialize the map -->
+			<script type="text/javascript">initMap();</script>
+			
+			<!-- Get the aurora data in JS -->
+			<script type="text/javascript">
+				var data = <?php include 'parseData.php';?>;
+				renderData(data);
+			</script>
+		</div>
 		
-		<!-- Initialize the map -->
-		<script type="text/javascript">initMap();</script>
-		
-		<!-- Get the aurora data in JS -->
-		<script type="text/javascript">
-			var data = <?php include 'parseData.php';?>;
-			renderData(data);
-		</script>
-		
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
     </body>
 </html>
